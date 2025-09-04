9262fe6d70d16f14bcfc6f058c3862f660007a7c
christianjulianblq <zombergamer678@gmail.com>
2025-09-04 14:44:30 -0600

    Update script_google_sheets.js

--- script_google_sheets.js
+++ script_google_sheets.js
@@ -3,10 +3,10 @@ function doPost(e) {
   var ss = SpreadsheetApp.getActiveSpreadsheet();
   if (data.tipo === "registro_qr") {
     var sheet1 = ss.getSheetByName("registro_qr") || ss.insertSheet("registro_qr");
-    sheet1.appendRow([new Date(), data.nombre, data.correo , data.telefono , data.motivo , data.delegacion , data.negocio , data.departamento , data.hora_entrada, data.hora_salida, data.qr_id]);
+    sheet1.appendRow([new Date(), data.nombre, data.correo , data.departamento , data.hora_entrada, data.hora_salida, data.qr_id,  data.telefono , data.motivo , data.delegacion , data.negocio]);
   } else if (data.tipo === "registro_visitantes") {
     var sheet2 = ss.getSheetByName("registro_visitantes") || ss.insertSheet("registro_visitantes");
-    sheet2.appendRow([new Date(), data.nombre, data.correo, data.fecha_nac, data.domicilio, data.telefono, data.motivo, data.delegacion, data.negocio , data.departamento, data.hora_entrada, data.hora_salida, data.qr_id]);
+    sheet2.appendRow([new Date(), data.nombre, data.correo, data.fecha_nac, data.domicilio, data.telefono, data.departamento, data.hora_entrada, data.hora_salida, data.qr_id,  data.motivo, data.delegacion, data.negocio]);
   }
   return ContentService.createTextOutput("OK");
 }
