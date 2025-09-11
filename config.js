a38242db0e04b03f1de8a42868beaf9e81676ce5
christianjulianblq <zombergamer678@gmail.com>
2025-09-10 20:35:53 -0600

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ const USERNAME = "admin";
 const PASSWORD = "1234";
 const SUPABASE_URL = "https://gtruqvwrsmhhnflwtxdt.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0cnVxdndyc21oaG5mbHd0eGR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU4MDM4ODQsImV4cCI6MjA3MTM3OTg4NH0.ZqIINGhpbtgAhyt_-T5KKmWeITbqzX5lp3VtSlDCkRk";
-const GOOGLE_SHEETS_WEBHOOK = "https://script.google.com/macros/s/AKfycbyCIHzuPIzf_aoBuydamkv76mLWi26ssrURH2fYcD9NzAReCyLswvpvkXToqiGh0pU/exec";
+const GOOGLE_SHEETS_WEBHOOK = "https://script.google.com/macros/s/AKfycbwjh0BjaeU9lrdaAbRj6LsyW9B7QEFsRBAsqrMPTnjFInyOMXetk7tp4hAaV3InLqAs/exec";
